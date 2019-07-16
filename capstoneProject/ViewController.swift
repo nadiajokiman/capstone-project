2350054f2bd019a597b65e283a9317443db3b95f
GWC <gwc@mains-MacBook-Air.local>
2019-07-16 11:10:26 -0700

    First commit

---
+++ capstoneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  capstoneProject
+//
+//  Created by GWC on 7/15/19.
+//  Copyright © 2019 GWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
